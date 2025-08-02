c5557ca6d491aae6d2af76e89a65d582100fb32f
nurgitlab <nurbek.math@gmail.com>
2025-08-02 19:00:39 +0300

    feat: hello axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3010
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3010").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
